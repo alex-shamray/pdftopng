e75c08c75e28f92e2931cf80b206b73fb95e6b3e
Alex Shamray <oleksiy.shamray@gmail.com>
2023-05-06 16:14:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -180,6 +180,8 @@ def setup_package():
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
         ],
         cmdclass={"build_ext": BuildExt},
     )
